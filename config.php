1fe0318ae81ec89f47a836f5aef05c6e59e1b115
frongotaku <61737028+frongotaku@users.noreply.github.com>
2020-09-28 14:11:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "us-cdbr-east-02.cleardb.com";
-$user = "b180ac2be6fe66";
-$password = "75d06d84";
-$database = "heroku_ca4ecde9fd95ba6";
+$host = "";
+$user = "";
+$password = "";
+$database = "";
 $conn = new mysqli($host, $user, $password, $database);
 mysqli_set_charset($conn, "utf8");
 date_default_timezone_set("Asia/Bangkok");
